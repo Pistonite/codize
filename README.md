afd75191273a45717ba66737e805ae705abce9a9
Michael Zhao <44533763+Pistonight@users.noreply.github.com>
2024-05-05 00:50:45 -0700

    Rewrite the library and add list feature (#2)
    * rewrite the library to be better
    
    * cargo fix stuff
    
    * add reallocation check

--- README.md
+++ README.md
@@ -1,17 +1,72 @@
 # codize
 
-A simple library that helps with printing code as strings
+![Build Badge](https://img.shields.io/github/actions/workflow/status/Pistonite/codize/rust.yml)
+![Version Badge](https://img.shields.io/crates/v/codize)
+![License Badge](https://img.shields.io/github/license/Pistonite/codize)
+![Issue Badge](https://img.shields.io/github/issues/Pistonite/codize)
+
+Simple, language-agnostic library that pretty-prints code for your code-generation tool.
+
+First, create a [`Code`] enum with the code structure with one of the ways listed below.
+Then, the [`Format`] struct can be used to format the output. Or you can simply use `to_string`
+for a quick formatting with the default parameters
+
+## [`Code`] Examples
+The [`Code`] enum stores all of the code structures. You can create it in one of the following
+ways:
+- Create a single line from a [`String`] or `&str` with `into()`
+- A block of code with an indented body with the [`cblock!`] macro
+- A list of code segments with a separator with the [`clist!`] macro
+- A concatenation of multiple code segments, either converted from an iterator with `into()`,
+or with the [`cconcat!`] macro which allows for mixing different types of code segments
+
+Usually, the macros will automatically convert the input to [`Code`] by calling `Code::from`.
 
 ```rust
-use codize::{codeln, block};
+use codize::{cblock, clist, cconcat};
 
-let expected = r"
+let code = cconcat![
+    "",
+    "/// This block is auto-generated",
+    "",
+    cblock!("fn main() {", [
+        cblock!("println!(", [
+            clist!("," => [r#""{}, {}!""#, r#""Hello""#, r#""world!""#])
+        ], ");")
+    ], "}"),
+];
+
+let expected = r#"
+/// This block is auto-generated
+
 fn main() {
-    foo();
-}";
+    println!(
+        "{}, {}!",
+        "Hello",
+        "world!",
+    );
+}"#;
 
-let code = block!("\nfn main() {", [
-   codeln!("foo();"),
-], "}");
 assert_eq!(expected, code.to_string());
+
+```
+
+## [`Format`] Examples
+You can use the [`FormatCode`] trait along with the [`Format`] struct to change global
+formatting options
+```rust
+use codize::{cblock, Format, FormatCode};
+let code = cblock!("fn main() {", ["println!(\"Hello, world!\");"], "}");
+
+let indent_2 = 
+r#"fn main() {
+  println!("Hello, world!");
+}"#;
+assert_eq!(indent_2, code.format_with(&Format::indent(2)));
+
+let indent_tab =
+"fn main() {
+\tprintln!(\"Hello, world!\");
+}";
+assert_eq!(indent_tab, code.format_with(&Format::indent_tab()));
 ```
--- src/block.rs
+++ src/block.rs
@@ -1,210 +1,286 @@
-/// Macro for creating a code block
+use crate::{Code, Concat, Format, FormatCode};
+
+/// A block of code with a starting line, ending line, and an indented body
+#[derive(derivative::Derivative)]
+#[derivative(Debug, PartialEq)]
+pub struct Block {
+    /// If this block should be connected to the end of a previous block
+    /// (for example, `else {`)
+    pub connect: bool,
+    /// The start of the block (for example, `if (x) {`)
+    pub start: String,
+    /// The end of the block (for example, `}`)
+    pub end: String,
+    /// The body of the block. Usually the body is the part that gets indented
+    concat_body: Concat,
+    /// When to inline
+    #[derivative(Debug = "ignore", PartialEq = "ignore")]
+    inline_condition: Option<fn(&Block) -> bool>,
+}
+
+impl Block {
+    /// Create a new block with empty body
+    pub fn empty<TStart, TEnd>(start: TStart, end: TEnd) -> Self
+    where
+        TStart: ToString,
+        TEnd: ToString,
+    {
+        Self {
+            connect: false,
+            start: start.to_string(),
+            concat_body: Concat::empty(),
+            end: end.to_string(),
+            inline_condition: None,
+        }
+    }
+
+    /// Create a new code block
+    pub fn new<TStart, TBody, TEnd>(start: TStart, body: TBody, end: TEnd) -> Self
+    where
+        TStart: ToString,
+        TEnd: ToString,
+        TBody: IntoIterator,
+        TBody::Item: Into<Code>,
+    {
+        Self {
+            connect: false,
+            start: start.to_string(),
+            concat_body: Concat::new(body),
+            end: end.to_string(),
+            inline_condition: None,
+        }
+    }
+
+    /// Set this block to start on the same line as the end of the previous block
+    pub fn connected(mut self) -> Self {
+        self.connect = true;
+        self
+    }
+
+    /// Set a condition for displaying the block as one line
+    pub fn inline_when(mut self, condition: fn(&Block) -> bool) -> Self {
+        self.inline_condition = Some(condition);
+        self
+    }
+
+    /// Set the inline condition to be always true
+    pub fn inlined(mut self) -> Self {
+        self.inline_condition = Some(|_| true);
+        self
+    }
+
+    /// Get the body of the block
+    #[inline]
+    pub fn body(&self) -> &[Code] {
+        &self.concat_body
+    }
+
+    /// Should the block be displayed in one line
+    pub fn should_inline(&self) -> bool {
+        if let Some(condition) = self.inline_condition {
+            condition(self)
+        } else {
+            self.should_inline_intrinsic()
+        }
+    }
+
+    /// Should intrinsicly inline the block
+    ///
+    /// This is used for blocks that only contain one line of code
+    pub fn should_inline_intrinsic(&self) -> bool {
+        self.body().len() == 1 && self.body()[0].should_inline()
+    }
+}
+
+impl From<Block> for Code {
+    fn from(x: Block) -> Self {
+        Code::Block(Box::new(x))
+    }
+}
+
+impl std::fmt::Display for Block {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.format())
+    }
+}
+
+impl FormatCode for Block {
+    fn size_hint(&self) -> usize {
+        // add the body, start, and end
+        self.concat_body.size_hint() + 2
+    }
+
+    fn format_into_vec_with(
+        &self,
+        format: &Format,
+        out: &mut Vec<String>,
+        connect: bool,
+        indent: &str,
+    ) {
+        let connect = self.connect || connect;
+        crate::append_line(out, &self.start, connect, indent);
+        let should_inline = self.should_inline();
+
+        if should_inline {
+            for code in self.body() {
+                code.format_into_vec_with(format, out, true, indent);
+            }
+        } else {
+            // indent the body
+            let i = format.indent;
+            let new_indent = if i < 0 {
+                format!("\t{indent}")
+            } else {
+                let i = i as usize;
+                format!("{:i$}{indent}", "")
+            };
+            for code in self.body() {
+                code.format_into_vec_with(format, out, false, &new_indent);
+            }
+        }
+        crate::append_line(out, &self.end, should_inline, indent);
+    }
+}
+
+/// Macro for creating [`Block`]s
 ///
 /// # Examples
 ///
-/// Single-line
-///
 /// ```
-/// use codize::{codeln, block};
+/// use codize::cblock;
 ///
-/// let expected = r"
-/// fn main() {
+/// let expected =
+/// "fn main() {
 ///     foo();
 /// }";
 ///
-/// let code = block!("\nfn main() {", [
-///    codeln!("foo();"),
+/// let code = cblock!("fn main() {", [
+///    "foo();",
 /// ], "}");
 /// assert_eq!(expected, code.to_string());
 ///
 /// ```
 ///
-/// Nested
-/// ```
-/// use codize::{codeln, block, Codize};
+/// Anything that implements `Into<Code>` can be used in the body.
+/// It can also be anything that implements `IntoIterator` and returns `Into<Code>`.
 ///
-/// let expected = r"
-/// fn foo(y: bool) {
-///   if (x()) {
-///     bar();
-///   } else if (y) {
-///     baz();
-///   }
-/// }
-/// ";
+/// You can call [`Block::connected`] to connect the start of the block with the end of the last block,
+/// such as an `else` block.
+/// ```
+/// use codize::cblock;
+///
+/// let expected =
+/// "fn foo(y: bool) {
+///     if (x()) {
+///         bar();
+///     } else if (y) {
+///         baz();
+///     }
+/// }";
 ///
 /// let func = "x";
-/// let code = block!("\nfn foo(y: bool) {", [
-///    block!(format!("if ({func}()) {{"), [
-///       codeln!("bar();"),
+/// let code = cblock!("fn foo(y: bool) {", [
+///    cblock!(format!("if ({func}()) {{"), [
+///       "bar();",
 ///    ], "}"),
-///    block!(> "else if (y) {", [
-///       codeln!("baz();")
-///    ], "}"),
+///    cblock!("else if (y) {", [
+///       "baz();"
+///    ], "}").connected(),
 /// ], "}");
-/// assert_eq!(expected, code.to_string_with(&Codize::indent(2).set_trailing_newline(true)));
+/// assert_eq!(expected, code.to_string());
 ///
 /// ```
 #[macro_export]
-macro_rules! block {
-    ($start:literal, [ $( $body:expr ),* $(,)? ] , $end:literal) => {
-        $crate::Code::block($crate::Block {
-            connect: false,
-            start: $start.to_owned(),
-            body: vec![ $( $body ),* ],
-            end: $end.to_owned(),
-        })
-    };
-    (> $start:literal, [ $( $body:expr ),* $(,)? ] , $end:literal) => {
-        $crate::Code::block($crate::Block {
-            connect: true,
-            start: $start.to_owned(),
-            body: vec![ $( $body ),* ],
-            end: $end.to_owned(),
-        })
-    };
-    ($start:expr, [ $( $body:expr ),* $(,)? ] , $end:literal) => {
-        $crate::Code::block($crate::Block {
-            connect: false,
-            start: $start,
-            body: vec![ $( $body ),* ],
-            end: $end.to_owned(),
-        })
-    };
-    (> $start:expr, [ $( $body:expr ),* $(,)? ] , $end:literal) => {
-        $crate::Code::block($crate::Block {
-            connect: true,
-            start: $start,
-            body: vec![ $( $body ),* ],
-            end: $end.to_owned(),
-        })
-    };
-    ($start:literal, [ $( $body:expr ),* $(,)? ] , $end:expr) => {
-        $crate::Code::block($crate::Block {
-            connect: false,
-            start: $start.to_owned(),
-            body: vec![ $( $body ),* ],
-            end: $end,
-        })
-    };
-    (> $start:literal, [ $( $body:expr ),* $(,)? ] , $end:expr) => {
-        $crate::Code::block($crate::Block {
-            connect: true,
-            start: $start.to_owned(),
-            body: vec![ $( $body ),* ],
-            end: $end,
-        })
+macro_rules! cblock {
+    ($start:expr, [] , $end:expr) => {
+        $crate::Block::empty($start, $end)
     };
     ($start:expr, [ $( $body:expr ),* $(,)? ] , $end:expr) => {
-        $crate::Code::block($crate::Block {
-            connect: false,
-            start: $start,
-            body: vec![ $( $body ),* ],
-            end: $end,
-        })
+        $crate::Block::new($start, [ $($crate::Code::from($body)),* ], $end)
     };
-    (> $start:expr, [ $( $body:expr ),* $(,)? ] , $end:expr) => {
-        $crate::Code::block($crate::Block {
-            connect: true,
-            start: $start,
-            body: vec![ $( $body ),* ],
-            end: $end,
-        })
+    ($start:expr, $body:expr, $end:expr) => {
+        $crate::Block::new($start, $body, $end)
     };
 }
 
-/// Macro for creating a code block with a vector as body
-///
-/// # Example
-/// ```
-/// use codize::{codeln, block, dynblock, Codize};
-///
-/// let expected = r"
-/// fn foo(y: bool) {
-///   if (x()) {
-///     bar();
-///   } else if (y) {
-///     baz();
-///   }
-/// }
-/// ";
-///
-/// let body = vec![
-///    block!(format!("if (x()) {{"), [
-///       codeln!("bar();"),
-///    ], "}"),
-///    block!(> "else if (y) {", [
-///       codeln!("baz();")
-///    ], "}"),
-/// ];
-/// let code = dynblock!("\nfn foo(y: bool) {", body, "}");
-/// assert_eq!(expected, code.to_string_with(&Codize::indent(2).set_trailing_newline(true)));
-///
-/// ```
-#[macro_export]
-macro_rules! dynblock {
-    ($start:literal, $body:expr, $end:literal) => {
-        $crate::Code::block($crate::Block {
-            connect: false,
-            start: $start.to_owned(),
-            body: $body,
-            end: $end.to_owned(),
-        })
-    };
-    (> $start:literal, $body:expr, $end:literal) => {
-        $crate::Code::block($crate::Block {
-            connect: true,
-            start: $start.to_owned(),
-            body: $body,
-            end: $end.to_owned(),
-        })
-    };
-    ($start:expr, $body:expr, $end:literal) => {
-        $crate::Code::block($crate::Block {
-            connect: false,
-            start: $start,
-            body: $body,
-            end: $end.to_owned(),
-        })
-    };
-    (> $start:expr, $body:expr, $end:literal) => {
-        $crate::Code::block($crate::Block {
-            connect: true,
-            start: $start,
-            body: $body,
-            end: $end.to_owned(),
-        })
-    };
-    ($start:literal, $body:expr, $end:expr) => {
-        $crate::Code::block($crate::Block {
-            connect: false,
-            start: $start,
-            body: $body,
-            end: $end,
-        })
-    };
-    (> $start:literal, $body:expr, $end:expr) => {
-        $crate::Code::block($crate::Block {
-            connect: true,
-            start: $start.to_owned(),
-            body: $body,
-            end: $end,
-        })
-    };
-    ($start:expr, $body:expr, $end:expr) => {
-        $crate::Code::block($crate::Block {
-            connect: false,
-            start: $start,
-            body: $body,
-            end: $end,
-        })
-    };
-    (> $start:expr, $body:expr, $end:expr) => {
-        $crate::Code::block($crate::Block {
-            connect: true,
-            start: $start,
-            body: $body,
-            end: $end,
-        })
-    };
+#[cfg(test)]
+mod test {
+    use indoc::indoc;
+
+    #[test]
+    fn empty() {
+        let code = cblock!("", [], "");
+        // start and end on separate lines
+        assert_eq!("\n", code.to_string());
+    }
+
+    #[test]
+    fn empty_body() {
+        let code = cblock!("fn main() {", [], "}");
+        assert_eq!("fn main() {\n}", code.to_string());
+    }
+
+    #[test]
+    fn different_types() {
+        let code = cblock!(
+            "fn main() {",
+            [
+                "foo",
+                "bar".to_string(),
+                cblock!("if (x) {", ["baz", "qux".to_string(),], "}"),
+            ],
+            "}"
+        );
+        let expected = indoc! {"
+            fn main() {
+                foo
+                bar
+                if (x) {
+                    baz
+                    qux
+                }
+            }"};
+        assert_eq!(expected, code.to_string());
+    }
+
+    #[test]
+    fn iteratable() {
+        let body = vec![
+            cblock!("if (x()) {", ["bar();"], "}"),
+            cblock!("else if (y) {", ["baz();"], "}").connected(),
+        ];
+        let code = cblock!("fn foo(y: bool) {", body, "}");
+        let expected = indoc! {"
+            fn foo(y: bool) {
+                if (x()) {
+                    bar();
+                } else if (y) {
+                    baz();
+                }
+            }"};
+        assert_eq!(expected, code.to_string());
+    }
+
+    fn is_one_thing(block: &crate::Block) -> bool {
+        block.body().len() == 1
+    }
+
+    #[test]
+    fn inline_condition() {
+        let body = vec![
+            cblock!("if (x()) {", ["bar();"], "}").inline_when(is_one_thing),
+            cblock!("else if (y) {", ["baz();", "baz();"], "}")
+                .connected()
+                .inline_when(is_one_thing),
+        ];
+        let code = cblock!("fn foo(y: bool) {", body, "}");
+        let expected = indoc! {"
+            fn foo(y: bool) {
+                if (x()) { bar(); } else if (y) {
+                    baz();
+                    baz();
+                }
+            }"};
+        assert_eq!(expected, code.to_string());
+    }
 }
